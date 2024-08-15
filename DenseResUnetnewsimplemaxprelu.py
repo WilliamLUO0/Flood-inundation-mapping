47a3e0468dc208d339f10024ad187a460679835c
songwk0924 <157378341+songwk0924@users.noreply.github.com>
2024-08-15 10:18:41 +0800

    Add files via upload

---
+++ DenseResUnetnewsimplemaxprelu.py
@@ -0,0 +1,167 @@
+# Rapid mapping of flood inundation by deep learning-based image super-resolution
+# Developer: Wenke Song
+# The University of Hong Kong
+# Contact email: songwk@connect.hku.hk
+# MIT License
+# Copyright (c) 2024 songwk0924
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResDenseBlocksimpler(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResDenseBlocksimpler,self).__init__()
+        self.BN1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.BN2 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+        self.prelu = nn.PReLU()
+        self.conv1x1_1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv1x1_2 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        
+
+    def forward(self, x):
+        input = x
+
+        x1 = self.BN1(x)
+        x1 = F.relu(x1)
+        x1 = self.conv1(x1)
+
+        add1_1 = self.conv1x1_1(input)
+        x2 = x1 + add1_1
+
+        x2 = self.BN2(x2)
+        x2 = F.relu(x2)
+        x2 = self.conv2(x2)
+
+        add2_1 = self.conv1x1_2(input)
+
+        x3 = x1 + x2 + add2_1
+
+        return x3
+
+class UpsampleLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UpsampleLayer,self).__init__()
+        self.BN = nn.BatchNorm2d(in_channels)
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2, padding=0)
+        self.prelu = nn.PReLU()
+
+    def forward(self, x, skip_connection):
+        x = self.BN(x)
+        x = F.relu(x)
+        x = self.up(x)
+        x = torch.cat([x, skip_connection], dim=1)
+        return x
+
+class conv1x1(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(conv1x1,self).__init__()
+        self.BN = nn.BatchNorm2d(in_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.prelu = nn.PReLU()
+
+    def forward(self, x):
+        x = self.BN(x)
+        x = F.relu(x)
+        x = self.conv(x)
+        return x
+    
+class MaskLayer(nn.Module):
+    def __init__(self):
+        super(MaskLayer, self).__init__()
+
+    def forward(self, x, mask):
+        mask = mask.unsqueeze(1)
+        x = x * mask
+        x = torch.clamp(x, min=0)
+        return x
+        
+class DenseResUnetsimple(nn.Module):
+    def __init__(self, in_channels, growth_rate):
+        super(DenseResUnetsimple,self).__init__()
+        self.BN1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels, growth_rate, kernel_size=3, stride=1, padding=1)
+        self.BN2 = nn.BatchNorm2d(growth_rate)
+        self.conv2 = nn.Conv2d(growth_rate, growth_rate, kernel_size=3, stride=1, padding=1)
+        
+        self.dense1 = ResDenseBlocksimpler(in_channels=growth_rate, out_channels= growth_rate*2)
+        self.dense2 = ResDenseBlocksimpler(in_channels=growth_rate*2, out_channels=growth_rate*4)
+        self.dense3 = ResDenseBlocksimpler(in_channels=growth_rate*4, out_channels= growth_rate*8)
+        self.dense4 = ResDenseBlocksimpler(in_channels=growth_rate*8, out_channels= growth_rate*16)
+    
+        self.upsample1 = UpsampleLayer(in_channels=growth_rate*16, out_channels=growth_rate*8)
+        self.dense5 = ResDenseBlocksimpler(in_channels=growth_rate*8*2, out_channels=growth_rate*8)
+    
+        self.upsample2 = UpsampleLayer(in_channels=growth_rate*8, out_channels=growth_rate*4)
+        self.dense6 = ResDenseBlocksimpler(in_channels=growth_rate*4*2, out_channels=growth_rate*4)
+        
+        self.upsample3 = UpsampleLayer(in_channels=growth_rate*4, out_channels=growth_rate*2)
+        self.dense7 = ResDenseBlocksimpler(in_channels=growth_rate*2*2, out_channels=growth_rate*2)
+
+        self.upsample4 = UpsampleLayer(in_channels=growth_rate*2, out_channels=growth_rate)
+        
+        self.BN3 = nn.BatchNorm2d(growth_rate*2)
+        self.conv5 = nn.Conv2d(growth_rate*2, growth_rate*2, kernel_size=3, stride=1, padding=1)
+        self.conv6 = conv1x1(in_channels=growth_rate*2, out_channels=1)
+
+        self.mask = MaskLayer()
+        self.prelu = nn.PReLU()
+
+    def forward(self, x, mask):
+
+        x = self.BN1(x)
+        x = F.relu(x)
+        x = self.conv1(x)
+        x = self.BN2(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+
+        x1 = self.dense1(x)
+        x1 = F.max_pool2d(x1, kernel_size=2, stride=2, padding=0)
+
+        x2 = self.dense2(x1)
+        x2 = F.max_pool2d(x2, kernel_size=2, stride=2, padding=0)
+    
+        x3 = self.dense3(x2)
+        x3 = F.max_pool2d(x3, kernel_size=2, stride=2, padding=0)
+
+        x4 = self.dense4(x3)
+        x4 = F.max_pool2d(x4, kernel_size=2, stride=2, padding=0)
+
+        x5 = self.upsample1(x4, x3)
+        x5 = self.dense5(x5)
+
+        x6 = self.upsample2(x5, x2)
+        x6 = self.dense6(x6)
+
+        x7 = self.upsample3(x6, x1)
+        x7 = self.dense7(x7)
+
+        x8 = self.upsample4(x7, x)
+        x8 = self.BN3(x8)
+        x8 = F.relu(x8)
+        x8 = self.conv5(x8)
+
+        x8 = self.conv6(x8)
+
+        x8 = self.mask(x8, mask)
+        return x8
+
+if __name__ == '__main__':
+    model1 = DenseResUnetsimple(in_channels=5, growth_rate=32)
+    msk = torch.randn(1,128,128)
+    input = torch.randn(1,5,128,128)
+
+    output1 = model1(input, msk)
+    print(output1.shape)
+        
+
+
+
+
+
+
---
+++ Prediction.py
@@ -0,0 +1,271 @@
+# Rapid mapping of flood inundation by deep learning-based image super-resolution
+# Developer: Wenke Song
+# The University of Hong Kong
+# Contact email: songwk@connect.hku.hk
+# MIT License
+# Copyright (c) 2024 songwk0924
+
+import torch
+import numpy as np
+import pandas as pd
+from DenseResUnetnewsimplemaxprelu import *
+import time
+
+start_time = time.time()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Prediction
+floodindex = 'v' # d -- max water depth, v -- max velocity
+coarsegrid = 80
+rainfallevent = 'r2'
+arch = 'DenseUnet'
+
+model = torch.load(f'./Trained_model/model_{floodindex}_{arch}.pth',map_location=device)
+
+nsize = 128
+overlap_rate = 0.5
+vminth = 0.1
+
+# Test cases
+data = np.load(f'./Test_cases/Test_{floodindex}_{rainfallevent}.npy')
+
+dataold = np.copy(data)
+bathy_patch = data[:, 0, :, :]
+mask_patch = (bathy_patch != 0)
+
+# Normalize data
+channels_to_normalize = [0, 1, 2, 3, 4]
+for c in channels_to_normalize:
+    min_val = data[:, c, ...].min(axis=(0, 1, 2), keepdims=True)
+    max_val = data[:, c, ...].max(axis=(0, 1, 2), keepdims=True)
+
+    if c == 2:
+        data[:, c, ...] = (data[:, c, ...]) / 6
+    elif c == channels_to_normalize[-1]:
+        min_val1 = np.array([[[0.]]])
+
+        if floodindex == 'd':
+            max_val1 = np.array([[[6.17581013]]]) # max water depth in the training data 
+        
+        elif floodindex == 'v':
+            max_val1 = np.array([[[6.89075068]]]) # max velocity in the training data
+
+        data[:, c, ...] = (data[:, c, ...] - min_val1) / (max_val1 - min_val1)
+    
+    else:
+        data[:, c, ...] = (data[:, c, ...] - min_val) / (max_val - min_val)
+
+
+data = torch.from_numpy(data) # {features}
+data = data.to(device).float()
+mask_patch = torch.from_numpy(mask_patch).to(device).float()
+
+# Load fine gird flood maps
+if floodindex == 'd':
+    finedata_path = f'./Test_cases/Fine_grid_flood_maps/hmax_{rainfallevent}.asc' 
+elif floodindex == 'v':
+    finedata_path = f'./Test_cases/Fine_grid_flood_maps/velmax_{rainfallevent}.asc'
+
+
+finedata_d = pd.read_csv(finedata_path,header=None)
+
+bathy_5m = np.array(pd.read_csv('./Test_cases/bathy_mat_5m_0p.csv', delimiter=' ',header=None))
+mask = np.array((bathy_5m != 0)).astype(int)
+
+fd = []
+for i in range(6, finedata_d.shape[0]):
+    row_d_values2 = finedata_d.iloc[i].values[0].split()
+    row_d_values2 = np.array(list(map(float, row_d_values2)))
+    
+    fd.append(row_d_values2)
+fd = np.array(fd)
+
+# prediction
+model.eval()
+output_list = []
+with torch.no_grad():
+    for i in range(data.shape[0]):
+        data_batch = data[i].unsqueeze(0)
+        
+        mask_batch = mask_patch[i].unsqueeze(0)
+        outputtemp = model(data_batch,mask_batch)
+        output_list.append(outputtemp)
+
+output = torch.cat(output_list, dim=0)
+
+row = 825
+col = 1310
+
+overlap_grid = np.ceil(nsize * overlap_rate).astype(int)
+ndown = nsize - overlap_grid
+
+if (row - overlap_grid) % ndown != 0:
+    npatch_row = (np.ceil((row - overlap_grid)/ndown)).astype(int)
+    nrow_add = (npatch_row*ndown + overlap_grid - row).astype(int)
+    n_up = np.ceil(nrow_add/2).astype(int)
+    n_down = (nrow_add - n_up).astype(int)
+
+if (col - overlap_grid) % ndown != 0:
+    npatch_col = (np.ceil((col - overlap_grid)/ndown)).astype(int)
+    ncol_add = (npatch_col*ndown + overlap_grid - col).astype(int)
+    n_left = np.ceil(ncol_add/2).astype(int)
+    n_right = (ncol_add - n_left).astype(int)
+
+big_image = np.zeros((row + nrow_add, col + ncol_add))
+cdimage = np.zeros((row + nrow_add, col + ncol_add))
+counter = np.zeros((row + nrow_add, col + ncol_add))
+
+for idx in range(npatch_row * npatch_col):
+    i = idx // npatch_col
+    j = idx % npatch_col
+    
+    start_i = i * ndown
+    start_j = j * ndown
+    
+    patch = output[idx, 0]
+    
+    big_image[start_i:start_i+nsize, start_j:start_j+nsize] += patch.cpu().numpy()
+    cdimage[start_i:start_i+nsize, start_j:start_j+nsize] += dataold[idx, channels_to_normalize[-1], :, :]
+    
+    counter[start_i:start_i+nsize, start_j:start_j+nsize] += 1
+
+big_image /= counter
+cdimage /= counter
+
+if n_up > 0:
+    big_image = big_image[n_up:, :]
+    cdimage = cdimage[n_up:, :]
+if n_down > 0:
+    big_image = big_image[:-n_down, :]
+    cdimage = cdimage[:-n_down, :]
+if n_left > 0:
+    big_image = big_image[:, n_left:]
+    cdimage = cdimage[:, n_left:]
+if n_right > 0:
+    big_image = big_image[:, :-n_right]
+    cdimage = cdimage[:, :-n_right]
+end_time = time.time()
+
+print(f"The code took {end_time - start_time} seconds to run.")
+
+# Analyze accuracy
+# 1. Classification performance
+# 1.1 RSME
+assert big_image.shape == fd.shape == mask.shape == cdimage.shape, "big_image, fd and mask must have the same shape"
+dff = fd * mask-big_image
+big_image_masked = big_image[mask == 1]
+fd_masked = fd[mask == 1]
+cd_masked = cdimage[mask == 1]
+diff = big_image_masked - fd_masked
+diff_sq = diff ** 2
+mean_diff_sq = np.sum(diff_sq)/np.sum(mask)
+rmse = np.sqrt(mean_diff_sq)
+print('Regression results')
+print('---------DL---------')
+print('RSME: {:.4f}'.format(rmse))
+
+mae = np.sum(np.abs(diff)) / np.sum(mask)
+print('MAE: {:.4f}'.format(mae))
+
+mean_observed = np.mean(fd_masked)
+sse = np.sum(diff_sq)
+sst = np.sum((fd_masked - mean_observed) ** 2)
+nse = 1 - (sse / sst)
+print('NSE: {:.4f}'.format(nse))
+
+# 1.2 Correlation coefficient
+corrcoef = np.corrcoef(big_image_masked.flatten(), fd_masked.flatten())[0, 1]
+print("PCC: {:.4f}".format(corrcoef))
+print('---------CD---------')
+diffcd = cd_masked - fd_masked
+diff_sqcd = diffcd ** 2
+mean_diff_sqcd = np.sum(diff_sqcd)/np.sum(mask)
+rmsecd = np.sqrt(mean_diff_sqcd)
+print('RSME: {:.4f}'.format(rmsecd))
+
+maecd = np.sum(np.abs(diffcd)) / np.sum(mask)
+print('MAE: {:.4f}'.format(maecd))
+
+mean_observedcd = np.mean(fd_masked)
+ssecd = np.sum(diff_sqcd)
+sstcd = np.sum((fd_masked - mean_observedcd) ** 2)
+nsecd = 1 - (ssecd / sstcd)
+print('NSE: {:.4f}'.format(nsecd))
+
+# 1.2 Correlation coefficient
+corrcoefcd = np.corrcoef(cd_masked.flatten(), fd_masked.flatten())[0, 1]
+print("PCC: {:.4f}".format(corrcoefcd))
+
+# 2. Regression performance
+# Accuracy interval
+def map_to_interval(x):
+
+    if x < 0.05:
+        return 1
+    elif  0.05<= x < 0.5:
+        return 2
+    elif  0.5 <= x < 1.5:
+        return 3
+    else:
+        return 4
+    
+def calculate_precision_recall(y_pred, y_true, class_label):
+    tp = np.sum((y_true == class_label) & (y_pred == class_label))
+    fp = np.sum((y_true != class_label) & (y_pred == class_label))
+    fn = np.sum((y_true == class_label) & (y_pred != class_label))
+
+    precision = tp / (tp + fp) if tp + fp > 0 else 0
+    recall = tp / (tp + fn) if tp + fn > 0 else 0
+    csi = tp / (tp + fn + fp) if tp + fn + fp > 0 else 0
+    f1 = 2 * (precision * recall) / (precision + recall) if precision + recall > 0 else 0
+
+    return precision, recall, csi, f1
+
+big_image_intervals = np.array(list(map(map_to_interval, big_image_masked)))
+cd_image_intervals = np.array(list(map(map_to_interval, cd_masked)))
+fd_intervals = np.array(list(map(map_to_interval, fd_masked)))
+same_elements = np.sum(big_image_intervals == fd_intervals)
+same_elementscd = np.sum(cd_image_intervals == fd_intervals)
+accuracybig = same_elements / len(big_image_intervals)
+accuracycd = same_elementscd / len(cd_image_intervals)
+
+# Calculate precision and recall for each class
+print('Classification results')
+print('---------DL---------')
+# Macro-averaging
+macro_precision = 0
+macro_recall = 0
+macro_csi = 0
+macro_f1 = 0
+num_classes = len(np.unique(big_image_intervals))
+
+micro_tp = 0
+micro_fp = 0
+micro_fn = 0
+
+for class_label in np.unique(big_image_intervals):
+    tp = np.sum((fd_intervals == class_label) & (big_image_intervals == class_label))
+    fp = np.sum((fd_intervals != class_label) & (big_image_intervals == class_label))
+    fn = np.sum((fd_intervals == class_label) & (big_image_intervals != class_label))
+    micro_tp += tp
+    micro_fp += fp
+    micro_fn += fn
+
+micro_precision = micro_tp / (micro_tp + micro_fp) if micro_tp + micro_fp > 0 else 0
+micro_recall = micro_tp / (micro_tp + micro_fn) if micro_tp + micro_fn > 0 else 0
+micro_csi = micro_tp / (micro_tp + micro_fn + micro_fp) if micro_tp + micro_fn + micro_fp > 0 else 0
+micro_f1 = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall) if micro_precision + micro_recall > 0 else 0
+
+print(f"Micro-averaged Precision = {micro_precision:.4f}, Recall = {micro_recall:.4f}, CSI = {micro_csi:.4f}, F1 = {micro_f1:.4f}")
+for class_label in np.unique(big_image_intervals):
+    precision, recall, csi, f1= calculate_precision_recall(big_image_intervals, fd_intervals, class_label)
+    print(f"Class {class_label}: Precision = {precision:.4f}, Recall = {recall:.4f}, CSI = {csi:.4f}, F1 = {f1:.4f}")
+
+print("Accuracy: {:.2%}".format(accuracybig))
+
+print('---------CD---------')
+for class_label in np.unique(big_image_intervals):
+    precision, recall, csi, f1= calculate_precision_recall(cd_image_intervals, fd_intervals, class_label)
+    print(f"Class {class_label}: Precision = {precision:.4f}, Recall = {recall:.4f}, CSI = {csi:.4f}, F1 = {f1:.4f}")
+
+print("Accuracy: {:.2%}".format(accuracycd))
